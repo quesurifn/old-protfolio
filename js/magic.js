528dcaf6b0b63cf23a2de44a986f56d4c3ce21d3
Kyle <kyle.c.r.fahey@gmail.com>
2017-02-01 17:32:16 -0600

    Fix Navbar

--- js/magic.js
+++ js/magic.js
@@ -11,40 +11,10 @@ $(function() {
       }
     }
   });
+});
 
-  $(window).on("load resize",function(e) {
-    var more = document.getElementById("js-centered-more");
 
-    if ($(more).length > 0) {
-      var windowWidth = $(window).width();
-      var moreLeftSideToPageLeftSide = $(more).offset().left;
-      var moreLeftSideToPageRightSide = windowWidth - moreLeftSideToPageLeftSide;
-
-      if (moreLeftSideToPageRightSide < 330) {
-        $("#js-centered-more .submenu .submenu").removeClass("fly-out-right");
-        $("#js-centered-more .submenu .submenu").addClass("fly-out-left");
-      }
-
-      if (moreLeftSideToPageRightSide > 330) {
-        $("#js-centered-more .submenu .submenu").removeClass("fly-out-left");
-        $("#js-centered-more .submenu .submenu").addClass("fly-out-right");
-      }
-    }
-
-    var menuToggle = $("#js-centered-navigation-mobile-menu").unbind();
-    $("#js-centered-navigation-menu").removeClass("show");
-
-    menuToggle.on("click", function(e) {
-      e.preventDefault();
-      $("#js-centered-navigation-menu").slideToggle(function(){
-        if($("#js-centered-navigation-menu").is(":hidden")) {
-          $("#js-centered-navigation-menu").removeAttr("style");
-          }
-      });
-    });
-  });
-
-  $(document).ready(function(){
+$(document).ready(function(){
 	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
 		$(this).toggleClass('open');
 	 });
@@ -56,12 +26,3 @@ $(function() {
            $body.scrollLeft(0);
        }
    });
-});
-
-jQuery(document).ready(function(){
-	jQuery('.skillbar').each(function(){
-		jQuery(this).find('.skillbar-bar').animate({
-			width:jQuery(this).attr('data-percent')
-		},6000);
-	});
-});
